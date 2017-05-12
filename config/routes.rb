75110b20bfccb30dfcfe35fe3039fb195174314a
Guillaume Jauffret <guillaumejfrt@gmail.com>
2017-05-12 10:05:47 +0200

    update fo the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'watches#index'
-  resources :watches
+  devise_for :users
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :watches
 end

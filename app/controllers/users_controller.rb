b9082a3a4fe47a32af0e68ea0fe5fd5eee07c7a0
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-12 15:30:22 +0200

    user controller ignit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,25 @@
 class UsersController < ApplicationController
-
+  before_action :find_user, only: : [ :show, :edit, :update]
   def show
     @user = current_user
   end
 
   def edit
+  @user = current_user
   end
+
+  def update
+    @user = User.new(user_params)
+  end
+
+  private
+
+  def find_user
+    @user = current_user
+  end
+
+  def user_params
+    require.(:user).permit(:first_name, :last_name)
+  end
+
 end

59e200c09758711306e6ee5a191aaf9230341bb9
Guillaume Jauffret <guillaumejfrt@gmail.com>
2017-05-15 12:18:33 +0200

    reset first_name and last_name default value to nil

--- db/migrate/20170515094613_add_default_value_to_user_first_name_and_last_name.rb
+++ db/migrate/20170515094613_add_default_value_to_user_first_name_and_last_name.rb
@@ -1,6 +1,11 @@
 class AddDefaultValueToUserFirstNameAndLastName < ActiveRecord::Migration[5.1]
-  def change
+  def up
     change_column_default :users, :first_name, ""
     change_column_default :users, :last_name, ""
+  end
+
+  def down
+    change_column_default :users, :first_name, default = nil
+    change_column_default :users, :last_name, default = nil
   end
 end

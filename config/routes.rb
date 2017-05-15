9f958a1e0ece6e2052639e2b25a1bff7a168f7c3
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-15 12:41:18 +0200

    Users routes show & edit deleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
+
   devise_for :users
 
   root 'pages#home'
   resources :watches do
     resources :rentals, only: [ :show, :create ]
   end
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [ :update ]
   get 'profile', to: 'pages#profile'
   get 'profile/edit', to: 'pages#edit_profile'
   get 'dashboard', to: 'pages#dashboard'

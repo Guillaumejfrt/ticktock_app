77680c86fe07d22148d009c38a67c520822c44ed
Guillaume Jauffret <guillaumejfrt@gmail.com>
2017-05-15 12:03:21 +0200

    callback before_save on update added to capitalize first_name and last_name

---
+++ db/migrate/20170515094613_add_default_value_to_user_first_name_and_last_name.rb
@@ -0,0 +1,6 @@
+class AddDefaultValueToUserFirstNameAndLastName < ActiveRecord::Migration[5.1]
+  def change
+    change_column_default :users, :first_name, ""
+    change_column_default :users, :last_name, ""
+  end
+end
